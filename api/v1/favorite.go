00fa8c661f60772d4daac5d624ffd2c34ceb8657
Yasin <yasindoyle97@gmail.com>
2026-05-20 17:20:24 +0800

    收藏夹

--- api/v1/favorite.go
+++ api/v1/favorite.go
@@ -1,21 +1,82 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/YasinDoyle/e-mall/consts"
+	"github.com/YasinDoyle/e-mall/service"
+	"github.com/YasinDoyle/e-mall/types"
+	"github.com/YasinDoyle/e-mall/utils/ctl"
+	"github.com/YasinDoyle/e-mall/utils/log"
+)
 
 // CreateFavoriteHandler 创建收藏
 func CreateFavoriteHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var req types.FavoriteCreateReq
+		if err := ctx.ShouldBind(&req); err != nil {
+			// 参数校验
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+
+		l := service.GetFavoriteSrv()
+		resp, err := l.FavoriteCreate(ctx.Request.Context(), &req)
+		if err != nil {
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+		ctx.JSON(http.StatusOK, ctl.RespSuccess(ctx, resp))
 	}
 }
 
 // ListFavoritesHandler 收藏夹详情接口
 func ListFavoritesHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var req types.FavoritesServiceReq
+		if err := ctx.ShouldBind(&req); err != nil {
+			// 参数校验
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+		if req.PageSize == 0 {
+			req.PageSize = consts.BasePageSize
+		}
+
+		l := service.GetFavoriteSrv()
+		resp, err := l.FavoriteList(ctx.Request.Context(), &req)
+		if err != nil {
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+		ctx.JSON(http.StatusOK, ctl.RespSuccess(ctx, resp))
 	}
 }
 
 // DeleteFavoriteHandler 删除收藏夹
 func DeleteFavoriteHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var req types.FavoriteDeleteReq
+		if err := ctx.ShouldBind(&req); err != nil {
+			// 参数校验
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+
+		l := service.GetFavoriteSrv()
+		resp, err := l.FavoriteDelete(ctx.Request.Context(), &req)
+		if err != nil {
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+		ctx.JSON(http.StatusOK, ctl.RespSuccess(ctx, resp))
 	}
 }
