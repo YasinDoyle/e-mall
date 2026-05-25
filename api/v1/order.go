1cbc64b3165f57194bf3b788f0e7e015ad56a389
Yasin <yasindoyle97@gmail.com>
2026-05-25 12:28:28 +0800

    订单

--- api/v1/order.go
+++ api/v1/order.go
@@ -1,24 +1,102 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/YasinDoyle/e-mall/consts"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/YasinDoyle/e-mall/service"
+	"github.com/YasinDoyle/e-mall/types"
+	"github.com/YasinDoyle/e-mall/utils/ctl"
+	"github.com/YasinDoyle/e-mall/utils/log"
+)
 
 func CreateOrderHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var req types.OrderCreateReq
+		if err := ctx.ShouldBind(&req); err != nil {
+			// 参数校验
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+
+		l := service.GetOrderSrv()
+		resp, err := l.OrderCreate(ctx.Request.Context(), &req)
+		if err != nil {
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+		ctx.JSON(http.StatusOK, ctl.RespSuccess(ctx, resp))
 	}
 }
 
 func ListOrdersHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var req types.OrderListReq
+		if err := ctx.ShouldBind(&req); err != nil {
+			// 参数校验
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+		if req.PageSize == 0 {
+			req.PageSize = consts.BasePageSize
+		}
+
+		l := service.GetOrderSrv()
+		resp, err := l.OrderList(ctx.Request.Context(), &req)
+		if err != nil {
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+		ctx.JSON(http.StatusOK, ctl.RespSuccess(ctx, resp))
 	}
 }
 
 // ShowOrderHandler 订单详情
 func ShowOrderHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var req types.OrderShowReq
+		if err := ctx.ShouldBind(&req); err != nil {
+			// 参数校验
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+
+		l := service.GetOrderSrv()
+		resp, err := l.OrderShow(ctx.Request.Context(), &req)
+		if err != nil {
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+		ctx.JSON(http.StatusOK, ctl.RespSuccess(ctx, resp))
 	}
 }
 
 func DeleteOrderHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var req types.OrderDeleteReq
+		if err := ctx.ShouldBind(&req); err != nil {
+			// 参数校验
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+
+		l := service.GetOrderSrv()
+		resp, err := l.OrderDelete(ctx.Request.Context(), &req)
+		if err != nil {
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK, ErrorResponse(ctx, err))
+			return
+		}
+		ctx.JSON(http.StatusOK, ctl.RespSuccess(ctx, resp))
 	}
 }
